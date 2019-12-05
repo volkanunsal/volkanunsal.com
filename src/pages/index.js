276482d97f69c9154ea660b61271141099b44c3b
Volkan Unsal <spocksplanet@gmail.com>
2019-12-04 19:34:14 -0500

    Start work on visual improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Nav from "../components/nav"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{ fontWeight: "bold", fontSize: "3rem", lineHeight: "3rem" }}>
-      VOLKAN UNSAL
-    </div>
-    <p style={{ fontSize: "2rem" }}>Full Stack Developer</p>
-    <Nav />
-  </Layout>
-)
+const IndexPage = () => {
+  useEffect(() => {}, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div style={{ fontWeight: "bold", fontSize: "3rem", lineHeight: "3rem" }}>
+        VOLKAN UNSAL
+      </div>
+      <p style={{ fontSize: "2rem" }}>Full Stack Developer</p>
+      <Nav />
+      <div className="art" />
+    </Layout>
+  )
+}
 
 export default IndexPage
