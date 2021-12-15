6640245c8dbabea517c558d500317dc19f9b717c
Volkan Unsal <spocksplanet@gmail.com>
2021-12-15 09:06:54 -0500

    Bring back art

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import Nav from "../components/nav"
 import SEO from "../components/seo"
+import art from "../lib/art"
 
 const IndexPage = () => {
+  useEffect(() => {
+    art()
+  }, [])
   return (
     <>
       <Layout>
@@ -16,6 +20,7 @@ const IndexPage = () => {
         <p style={{ fontSize: "2rem" }}>Full Stack Developer</p>
         <Nav />
       </Layout>
+      <div className="art-container" style={{ marginTop: "-3rem" }} />
     </>
   )
 }
