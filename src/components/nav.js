1da4359bef186c40adbf7ef01aab83928de519f2
Volkan Unsal <spocksplanet@gmail.com>
2021-12-15 10:55:12 -0500

    Update nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,35 +1,25 @@
-import { Link } from "gatsby"
 import React from "react"
 
-export default ({ showHome }) => {
+export default () => {
   return (
     <div className="d-flex mb-4">
-      <div className="d-flex">
-        {showHome && (
-          <Link to="/" className="mr-3">
-            <i className="fa fa-caret-left" /> Home
-          </Link>
-        )}
-      </div>
-      <div className="d-flex  border-left pl-3">
-        <a
-          className="mr-3"
-          href="https://github.com/volkanunsal"
-          target="__blank"
-        >
-          Github{" "}
-        </a>
-        <a
-          className="mr-3"
-          href="https://www.linkedin.com/in/volkanunsal"
-          target="__blank"
-        >
-          LinkedIn
-        </a>
-        <a href="mailto:spocksplanet@gmail.com?subject=hello" target="__blank">
-          Contact
-        </a>
-      </div>
+      <a
+        className="mr-3"
+        href="https://github.com/volkanunsal"
+        target="__blank"
+      >
+        Github{" "}
+      </a>
+      <a
+        className="mr-3"
+        href="https://www.linkedin.com/in/volkanunsal"
+        target="__blank"
+      >
+        LinkedIn
+      </a>
+      <a href="mailto:spocksplanet@gmail.com?subject=hello" target="__blank">
+        Contact
+      </a>
     </div>
   )
 }
